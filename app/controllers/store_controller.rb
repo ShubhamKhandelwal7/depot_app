0fb89ce065511dee6064c516cc1c63327062fa16
Shubham369 <shubham.khandelwal@vinsol.com>
2020-03-03 12:34:35 +0530

    Completed Task: E

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
+  # attr_accessor :visit_counter
+
+  # @@visit_counter ||= 0
   def index
     @products = Product.order(:title)
+    # p @@visit_counter
+    # @@visit_counter +=1
   end
 end
