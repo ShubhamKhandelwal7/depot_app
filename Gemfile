ec5c7a77d6f4744a533d40c4907766063bb2084d
Shubham369 <shubham.khandelwal@vinsol.com>
2020-03-05 19:36:52 +0530

    Completed Task: K

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.5'
 gem 'activemodel-serializers-xml'
 
 # Use Active Storage variant
@@ -53,3 +53,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'i18n-js'
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,12 +1,17 @@
 class StoreController < ApplicationController
+  skip_before_action :authorize 
   # attr_accessor :visit_counter
   include CurrentCart
   before_action :set_cart
 
   # @@visit_counter ||= 0
   def index
-    @products = Product.order(:title)
     # p @@visit_counter
     # @@visit_counter +=1
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+      else
+      @products = Product.order(:title)
+      end
   end
 end
